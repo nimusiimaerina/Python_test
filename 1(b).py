c5840cc1c3510f683aaaa06560ca3fac11cf270d
nimusiimaerina <nimusiimaerinah@gmail.com>
2024-10-21 11:30:16 +0300

    Update 1(b).py

--- 1(b).py
+++ 1(b).py
@@ -2,6 +2,6 @@ def print_multiplication_table(number):
     for i in range(1, 13):
         print(f"{number} x {i} = {number * i}")
 
-# multipication table of 5
+# multipication table 
 num = 5
 print_multiplication_table(num)
