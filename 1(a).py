58d22331e988eeffc4d2413607d42f4a79d80f14
nimusiimaerina <nimusiimaerinah@gmail.com>
2024-10-21 11:29:53 +0300

    Update 1(a).py

--- 1(a).py
+++ 1(a).py
@@ -1,7 +1,7 @@
 def get_even_numbers(numbers):
     return [num for num in numbers if num % 2 == 0]
 
-#Example to be in use
+
 input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even_numbers = get_even_numbers(input_list)
 print(even_numbers)
